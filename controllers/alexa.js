d30d1e24df16ef3625a68447997f9b7e7d872c1b
Will Melani <will_melani@gap.com>
2015-06-18 22:51:45 -0700

    Add dummy request

--- controllers/alexa.js
+++ controllers/alexa.js
@@ -4,7 +4,27 @@ var app = require('../app');
 var kodiService = require(app.dir + '/api/kodi-service.js').create({ host : process.env.KODI_HOST});
 
 function alexaPostRequest(req,response,done){
-    response.send("not implemented");
+    console.log(req.body);
+    var text= "test";
+    var obj = {
+        "version": "1.0",
+        "sessionAttributes": {
+        },
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": text
+            },
+            "card": {
+                "type": "Simple",
+                "title": "Kodi",
+                "content": text
+            },
+            "shouldEndSession": true
+        }
+    };
+    response.send(obj);
+
 }
 
 
